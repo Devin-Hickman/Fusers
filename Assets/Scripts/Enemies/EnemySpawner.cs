a1bc0f9c7da73e4f01a12d01fb6d2493dc02c799
Devin Hickman <dhickman811@gmail.com>
2018-10-29 18:53:38 -0700

    Added first enemy spawner test

--- Assets/Scripts/Enemies/EnemySpawner.cs
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -6,26 +6,44 @@ using Fusers;
 public class EnemySpawner : MonoBehaviour {
 
     private float timeBetweenEnemySpawns = 0.5f;
+    private float timeOfLastSpawn = 0;
+    private GameObject enemyToSpawn;
+
+    public GameObject EnemyToSpawn { get => enemyToSpawn; set => enemyToSpawn = value; }
 
     public void setTimeBetweenEnemySpawns(float time) { timeBetweenEnemySpawns = time; }
 
-	// Use this for initialization
-	void Start () {
-        StartCoroutine("SpawnEnemy");
-        
-		
-	}
+    //TODO: Add way to set what enemy type is spawned per level
+
+    void Update()
+    {
+        if(Time.time > timeOfLastSpawn + timeBetweenEnemySpawns)
+        {
+            SpawnEnemy(10);
+            timeOfLastSpawn = Time.time;
+        }
+
+    }
+
+    public void SpawnEnemy(int numEnemies)
+    {
+        for (int i = 0; i < numEnemies; i++)
+        {
+            GameObject enemy = (GameObject)Instantiate(Resources.Load("Base enemy"));
+            enemey.addComponent<enemyToSpawn>();
+            enemy.transform.position = this.transform.position;
+        }
+    }
 	
 
-    public IEnumerator SpawnEnemy(IEnemy enemyToSpawn)
+    public void SpawnEnemy(IEnemy enemyToSpawn, int numEnemies)
     {
-        for(int i = 0; i < 10; i++)
+        for(int i = 0; i < numEnemies; i++)
         {
             GameObject enemy = (GameObject) Instantiate(Resources.Load("Base enemy"));
             enemey.addComponent<enemyToSpawn>();
             enemy.transform.position = this.transform.position;
-            yield return new WaitForSeconds(0.5f);
         }
-
     }
+
 }
---
+++ Assets/Scripts/Tests/EnemySpawnerTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine.Testtools;
+
+namespace Tests
+{
+    public class EnemySpawnerTest
+    {
+        /// <summary>
+        /// Spawns a single enemy using the enemy spawner class
+        /// </summary>
+        /// <returns></returns>
+        [UnityTest]
+        public IEnumerator Spawn_Single_Enemy_Test()
+        {
+            EnemySpawner enemySpawner = new EnemySpawner();
+            GameObject enemyPrefab = (GameObject)Resources.Load("grunt");
+            enemySpawner.EnemyToSpawn = enemyPrefab;
+
+            yield return null;
+
+            var spawnedEnemy = GameObject.FindWithTag("Enemy");
+
+
+            Assert.AreEqual(enemyPrefab, spawnedEnemy);
+        }
+
+        [TearDown]
+        private void RemoveAllObjectsFromScence()
+        {
+            foreach(GameObject gameObject in GameObject.FindGameObjectsWithTag("Enemy"){
+                Object.Destroy(gameObject);
+            }
+            foreach(GameObject gameObject in Object.FindObjectOfType<EnemySpawner>)
+            {
+                Object.Destroy(gameObject);
+            }
+        }
+    }
+}
